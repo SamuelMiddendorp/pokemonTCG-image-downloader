1fada6d4b1fc6314ab23dc7e6fcb6b6a800c2c07
Samuel Middendorp <samiezaurus@gmail.com>
2023-07-31 22:54:26 +0200

    First check to download with axios

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log("foobar");
+const axios = require('axios');
+const fs = require('fs');
+const sampleUrl = "https://images.pokemontcg.io/base1/9.png";
+
+
+const main = async () => {
+    const res = await axios.get(sampleUrl, {
+        responseType: 'arraybuffer'
+    });
+    fs.writeFileSync("bar.png", res.data);
+}
+main();
