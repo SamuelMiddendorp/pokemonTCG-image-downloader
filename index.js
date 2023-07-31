e248e7aafb6d1c965a60036408ef14c16e20c05a
Samuel Middendorp <samiezaurus@gmail.com>
2023-07-31 23:16:01 +0200

    Add sample set

--- index.js
+++ index.js
@@ -1,12 +1,27 @@
 const axios = require('axios');
-const fs = require('fs');
-const sampleUrl = "https://images.pokemontcg.io/base1/9.png";
+const fse = require('fs-extra');
+const sampleSet = "sm2";
 
+const getImagesFromSet = async (set) => {
 
+    const {
+        data
+    } = await axios.get(`https://api.pokemontcg.io/v2/cards?q=set.id:${set}&page=1&pageSize=250&orderBy=number`);
+    return data.data.map(x => x.images.small);
+}
 const main = async () => {
-    const res = await axios.get(sampleUrl, {
-        responseType: 'arraybuffer'
-    });
-    fs.writeFileSync("bar.png", res.data);
+
+    // Load image urls from given set;
+    const images = await getImagesFromSet(sampleSet);
+    for (var i = 0; i < images.length; i++) {
+
+        let image = images[i];
+        const res = await axios.get(image, {
+            responseType: 'arraybuffer'
+        });
+        console.log(image);
+        // Get correct folder and file name from image url
+        fse.outputFile(image.split("pokemontcg.io/")[1], res.data);
+    }
 }
 main();
