a282f1f6188a28553aabd7360023deeba98eace3
Samuel Middendorp <samiezaurus@gmail.com>
2023-08-01 14:43:18 +0200

    Add input possibilities and pagination

--- index.js
+++ index.js
@@ -1,27 +1,63 @@
 const axios = require('axios');
 const fse = require('fs-extra');
-const sampleSet = "sm2";
 
-const getImagesFromSet = async (set) => {
+const getImagesFromSet = async (set, useLargeImages) => {
 
-    const {
-        data
-    } = await axios.get(`https://api.pokemontcg.io/v2/cards?q=set.id:${set}&page=1&pageSize=250&orderBy=number`);
-    return data.data.map(x => x.images.small);
+    // Inititalize here
+    let images = []
+    const data = await doRequest(formulatePaginatedRequest(1, set));
+
+    images = images.concat(data.data)
+    // Anno 2023 some sets are very big, luckily they are not that big that we have to recursively use pages
+    if (data.totalCount > 250) {
+        const data = await doRequest(formulatePaginatedRequest(2, set));
+        images = images.concat(data.data);
+    }
+    // For now small images are returned
+    return images.map(x => useLargeImages ? x.images.large : x.images.small);
 }
+const doRequest = async (url) => {
+
+    const { data } = await axios.get(url);
+
+    if (data.data.length == 0) {
+        throw new Error("Api call yielded no results, is the setId correct?");
+    } else {
+        return data;
+    }
+}
+const formulatePaginatedRequest = (page, set) => {
+    return `https://api.pokemontcg.io/v2/cards?q=set.id:${set}&page=${page}&pageSize=250&orderBy=number&select=images`
+}
+
 const main = async () => {
 
+    console.log("Thanks for using this tool, be mindfull when running large exports since images take quite some bandwidth!\nPress CTRL + c to stop at anytime");
+    
+    const setId = process.argv[2] || "";
+    const useLargeImages = process.argv[3] || false;
+
+    if (setId == "") {
+        throw new Error("No set has been provided as input");
+    }
+
     // Load image urls from given set;
-    const images = await getImagesFromSet(sampleSet);
+    const images = await getImagesFromSet(setId, useLargeImages);
+
     for (var i = 0; i < images.length; i++) {
 
         let image = images[i];
+        // Read image as data
         const res = await axios.get(image, {
             responseType: 'arraybuffer'
         });
-        console.log(image);
         // Get correct folder and file name from image url
-        fse.outputFile(image.split("pokemontcg.io/")[1], res.data);
+        const outputPath = image.split("pokemontcg.io/")[1];
+
+        console.log(`Writing file: ${outputPath}`);
+        // fse outputfile is used to leverage the same file structure of the source files. It creates folders if they do not exist
+        // normal fs writefile will not do this.
+        fse.outputFile(outputPath, res.data);
     }
 }
 main();
